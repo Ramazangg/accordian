500d1d09a79ec98ef3cf5d42f0820b28eec7acd9
Ramazangg <rmesutov@gmail.com>
2022-11-20 03:55:41 +0300

    Update scr.js

--- scr.js
+++ scr.js
@@ -1,5 +1,4 @@
-//uses classList, setAttribute, and querySelectorAll
-//if you want this to work in IE8/9 youll need to polyfill these
+
 (function(){
     var d = document,
     accordionToggles = d.querySelectorAll('.js-accordionTrigger'),
